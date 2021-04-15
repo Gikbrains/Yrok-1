905095997c8e09a0918b790eb107f1263205f96d
Gikbrains <79279039+Gikbrains@users.noreply.github.com>
2021-04-15 16:11:26 +0300

    Add files via upload

---
+++ Задание  2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            private static ISet<string> Compute(int n)
+            {
+                var set = new HashSet<string>();
+
+                for (int i = 2; i < n; i++)
+                    set.Add(ToBinaryString(i));
+
+                for (int step = 2; step < n; step++)
+                    for (int i = 2 * step; i < n; i += step)
+                        set.Remove(ToBinaryString(i));
+
+                return set;
+            }
+
+            private static string ToBinaryString(int x)
+            {
+                return x == 0 ? "0" : ToBinaryString(x / 2) + x % 2;
+            }
+        }
+    }
+}
