445b3b1436cfe4f49032ae830ae997976ca2dc4a
Gikbrains <79279039+Gikbrains@users.noreply.github.com>
2021-04-15 16:11:50 +0300

    Add files via upload

---
+++ Задание 3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testCase1 = new TestCase()
+            {
+                X = 8,
+                Y = 12,
+                Expected = 20,
+                ExpectedException = null
+            };
+
+            var testCase2 = new TestCase()
+            {
+                X = 4,
+                Y = 6,
+                Expected = 10,
+                ExpectedException = null
+            };
+
+            TestSum(testCase1);
+            TestSum(testCase2);
+
+        }
+    }
+}
